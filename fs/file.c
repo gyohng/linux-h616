3cee1f6581adc8967300ad8b815f9953c375d031
Philippe Gerum <rpm@xenomai.org>
2016-07-21 21:04:43 +0200

    dovetail: add core support
    This is the basic interface layer for hosting a high-priority resident
    co-kernel, aka "dovetailing".
    
    The co-kernel leverages the interrupt pipeline ability to turn common
    device IRQs into pseudo-NMIs from the perspective of the in-band code
    (CONFIG_IRQ_PIPELINE), to achieve very low latency for the co-kernel
    running on the oob stage.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>
    
    dovetail: Fix detection of prctl based compat syscalls
    
    In case a compat application is requesting a dovetail service the
    syscall number will not be equal to __NR_prctl (= the x86_64 syscall
    number).
    
    The same problem exists on the arm64 architecture where the problem was
    already addressed. Apply the same to x86.
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>
    
    dovetail: arch: Fix fetching of the first syscall argument from compat tasks
    
    On x86 there is a small difference in the ABI (= register) where the
    first syscall argument is supplied between the x86_64 (native) and
    compat (i386) case.
    
    On x86_64 the DI register is being used, while i386 used the BX
    register.
    
    The same problem exists for further arguments - handled by
    syscall_get_arguments() - as well. syscall_get_arg0() is dovetail
    specific and is now in sync with syscall_get_arguments().
    
    Signed-off-by: Florian Bezdeka <florian.bezdeka@siemens.com>

--- fs/file.c
+++ fs/file.c
@@ -429,6 +429,7 @@ static struct fdtable *close_files(struct files_struct * files)
 			if (set & 1) {
 				struct file * file = xchg(&fdt->fd[i], NULL);
 				if (file) {
+					uninstall_inband_fd(i, file, files);
 					filp_close(file, files);
 					cond_resched();
 				}
@@ -615,6 +616,7 @@ void fd_install(unsigned int fd, struct file *file)
 		fdt = files_fdtable(files);
 		BUG_ON(fdt->fd[fd] != NULL);
 		rcu_assign_pointer(fdt->fd[fd], file);
+		install_inband_fd(fd, file, files);
 		spin_unlock(&files->file_lock);
 		return;
 	}
@@ -623,6 +625,7 @@ void fd_install(unsigned int fd, struct file *file)
 	fdt = rcu_dereference_sched(files->fdt);
 	BUG_ON(fdt->fd[fd] != NULL);
 	rcu_assign_pointer(fdt->fd[fd], file);
+	install_inband_fd(fd, file, files);
 	rcu_read_unlock_sched();
 }
 
@@ -654,6 +657,7 @@ struct file *file_close_fd_locked(struct files_struct *files, unsigned fd)
 	if (file) {
 		rcu_assign_pointer(fdt->fd[fd], NULL);
 		__put_unused_fd(files, fd);
+		uninstall_inband_fd(fd, file, files);
 	}
 	return file;
 }
@@ -845,6 +849,7 @@ void do_close_on_exec(struct files_struct *files)
 				continue;
 			rcu_assign_pointer(fdt->fd[fd], NULL);
 			__put_unused_fd(files, fd);
+			uninstall_inband_fd(fd, file, files);
 			spin_unlock(&files->file_lock);
 			filp_close(file, files);
 			cond_resched();
@@ -1261,6 +1266,7 @@ __releases(&files->file_lock)
 		__set_close_on_exec(fd, fdt);
 	else
 		__clear_close_on_exec(fd, fdt);
+	replace_inband_fd(fd, file, files);
 	spin_unlock(&files->file_lock);
 
 	if (tofree)
--- include/linux/sched/coredump.h
+++ include/linux/sched/coredump.h
@@ -91,6 +91,7 @@ static inline int get_dumpable(struct mm_struct *mm)
 
 #define MMF_VM_MERGE_ANY	30
 #define MMF_VM_MERGE_ANY_MASK	(1 << MMF_VM_MERGE_ANY)
+#define MMF_DOVETAILED		31	/* mm belongs to a dovetailed process */
 
 #define MMF_INIT_MASK		(MMF_DUMPABLE_MASK | MMF_DUMP_FILTER_MASK |\
 				 MMF_DISABLE_THP_MASK | MMF_HAS_MDWE_MASK |\
